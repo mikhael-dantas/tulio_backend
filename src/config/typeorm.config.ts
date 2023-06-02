3a95396a7d7a2e38b289e15642ddcfed18dc3851
mikhael dantas <mikhael-dcs@outlook.com>
2023-06-02 18:12:14 -0300

    host update

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,7 +3,7 @@ import { User } from 'src/users/entities/user.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
+  host: 'mysql',
   port: 3306,
   username: 'root',
   password: 'root',
